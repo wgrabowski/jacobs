b0092f0fba20a67aace6a2dd33acb38b0021a68a
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2018-01-21 17:33:37 +0100

    Use per-repo files to provide job names. Track multiple jobs. Change output formatting

--- README.md
+++ README.md
@@ -7,12 +7,8 @@ jacobs check if your commits was processed by your Jenkins jobs
 # Before use / installation
 * clone repo or download zip to your machine
 * copy credentials.properties.sample as credentials.properties
-* in ```credentials.properties``` provide jenkins url, your username & password and name of job you want to track
+* in ```credentials.properties``` provide jenkins url, your username & password want to track
+* in each repository you want to track add ```.jacobsrc``` file with names of Jenkins jobs you want to track
 # Usage
 ## Alias
 It is recommended to add alias (i.e. in your .bashrc file) pointing to jacobs.sh location.
-
-## Check last N commits
-To check if last N commits was successfully built by your Jenkins job:
-```<your alias of path to jacobs.sh> N``` where N is number of your choice
-
--- jacobs.sh
+++ jacobs.sh
@@ -1,30 +1,56 @@
 #!/usr/bin/env bash
 JACOBS_HOME=$(dirname $0);
+REPO_HOME=$(git rev-parse --show-toplevel);
+declare -A last_revisions;
 source "$JACOBS_HOME"/credentials.properties;
 
+# $1 - job name
 get_last_built_revision(){
-curl -s -X GET $JENKINS_URL/job/$JENKINS_JOB_NAME/lastSuccessfulBuild/api/json \
+curl -s -X GET $JENKINS_URL/job/$1/lastSuccessfulBuild/api/json \
 --user $JENKINS_USER:$JENKINS_PASSWORD | jq -r '.actions[] | select(._class == "hudson.plugins.git.util.BuildData") | .lastBuiltRevision.SHA1'
 }
+get_last_revisions(){
+    for job in $(cat "$REPO_HOME/.jacobsrc") ;do
+    echo "Get last built revision for $job"
+    last_revisions[$job]=$(get_last_built_revision $job) 
+    done
+    }
+# $1 - commit hash
+check_commit(){
+    for job in ${!last_revisions[@]} ;do
+        git merge-base --is-ancestor $1 ${last_revisions[$job]}
+        if [ "$?" -eq "0" ]; then
+            built="\033[32mWAS_BUILT\e[0m";
+        else
+            built="\033[31mNOT_BUILT\e[0m";
+        fi
+        printf "\n\t %b %s %b" $built "on"  $job
+    done
+        
+    
+}
+# $1 - commit list 
+check_commits(){
+    # local last_build_rev=$(get_last_built_revision);
+    git fetch
+    get_last_revisions
 
-# $1 - number of last commits to check
-check_last_n_commits(){
-    local last_build_rev=$(get_last_built_revision);
-
-    printf "%30s \t %.40s" "Commit" "$JENKINS_JOB_NAME"
-    printf "\n---------------------------------------------------------------------"
+    printf "Commits:" 
     while read commit; do
         rev=$(echo "$commit" | cut -d" " -f 1);
         name=$(echo "$commit" | cut -d" " -f 2-);
-        git merge-base --is-ancestor $rev $last_build_rev
-        if [ "$?" -eq "0" ]; then
-            built="built";
-        else
-            built="not built";
-        fi
-        printf "\n%s %.22s \t  %s" $rev "$name" "$built"
-    done <<< "$(git log --oneline -$1)";
+        printf "\n\n%s %.80s \t  %s" $rev "$name"
+        check_commit $rev                 
+    done <<< "$1";
     printf "\n"
+}
+# $1 - number of last commits to check
+check_last_n_commits(){
+    check_commits "$(git log --oneline -$1)";
+}
+# $1 - pattern to filer by
+filter(){
+    check_commits "$(git log --oneline --grep="$1" -$2)";
 }
 usage(){
     echo 'jacobs - check if your commits were built by Jenkins
@@ -52,7 +78,15 @@ while (( "$#" )); do
     case $1 in
 		last)
 			check_last_n_commits $2
-			shift
+            shift
+			exit
+
+		;;
+        filter)
+			filter $2 $3
+            shift
+            shift
+			exit
 		;;
 		config)
 			config
